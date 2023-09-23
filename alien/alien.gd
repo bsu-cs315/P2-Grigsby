c2558306fc9fc6d2fae4f47c745f4af5100f42c2
Nathaniel Grigsby <ncgrigsby@bsu.edu>
2023-09-23 16:29:05 -0400

    Added platforms, player, and collectible

---
+++ alien/alien.gd
@@ -0,0 +1,41 @@
+extends CharacterBody2D
+
+
+const SPEED = 300.0
+const JUMP_VELOCITY = -550.0
+
+# Get the gravity from the project settings to be synced with RigidBody nodes.
+var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
+
+var keys := 0
+
+@onready var _animated_sprite := $AnimatedSprite2D
+
+func _physics_process(delta):
+	if not is_on_floor():
+		velocity.y += gravity * delta
+
+	# Handle Jump.
+	if Input.is_action_just_pressed("jump") and is_on_floor():
+		velocity.y = JUMP_VELOCITY
+
+	# Get the input direction and handle the movement/deceleration.
+	# As good practice, you should replace UI actions with custom gameplay actions.
+	var direction = Input.get_axis("left", "right")
+	if direction:
+		velocity.x = direction * SPEED
+	else:
+		velocity.x = move_toward(velocity.x, 0, SPEED)
+		
+	var animation_name := "walk" if velocity.x != 0 else "idle"
+	_animated_sprite.play(animation_name)
+	
+	if velocity.y != 0:
+		_animated_sprite.play("jump")
+	
+	if velocity.x > 0:
+		_animated_sprite.scale.x = 1
+	elif velocity.x < 0:
+		_animated_sprite.scale.x = -1
+
+	move_and_slide()
---
+++ objects/key.gd
@@ -0,0 +1,20 @@
+extends Area2D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+func _on_body_entered(body):
+	if body.is_in_group("aliens"):
+		body.keys += 1
+		
+		var audio_stream_player := AudioStreamPlayer.new()
+		audio_stream_player.stream = load("res://objects/keysound.wav")
+		get_parent().add_child(audio_stream_player)
+		audio_stream_player.play()
+		audio_stream_player.finished.connect(func():
+			audio_stream_player.queue_free()
+		)
+		
+		queue_free()
