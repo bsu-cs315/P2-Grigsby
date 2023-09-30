0ba2a117dc875ada94c57f3b63228906a670d2c3
Nathaniel Grigsby <ncgrigsby@bsu.edu>
2023-09-30 14:56:45 -0400

    More attempts at making the HUD work

--- hud/hud.gd
+++ hud/hud.gd
@@ -1,17 +1,19 @@
 extends Control
 
+var coin : Coin
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	$key.visible = false
-	
-func set_alien(alien):
-	alien.key_collected.connect(_on_key_collected)
+	pass
 	
 func _process(delta):
 	pass
 	
-func _on_key_collected(new_keys):
-	_update_key_label()
+func setCoin(new_coin) -> void:
+	new_coin.coin_collected.connect(_on_coin_collected)
+	$coin_label.text = "0"
+
+func _on_coin_collected(new_coins):
+	update_coin_label(new_coins)
 	
-func _update_key_label():
-	$key.visible = true
+func update_coin_label(new_coins):
+	$coin_label.text = str(new_coins)
--- objects/coin.gd
+++ objects/coin.gd
@@ -1,14 +1,16 @@
-extends Area2D
+class_name Coin extends Area2D
 
+signal coin_collected(new_coins)
 
+var coins := 0
+var original_coins := 0
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
 
 func _on_body_entered(body):
 	if body.is_in_group("aliens"):
-		body.keys += 1
-		
+		coins += 1
 		var audio_stream_player := AudioStreamPlayer.new()
 		audio_stream_player.stream = load("res://objects/keysound.wav")
 		get_parent().add_child(audio_stream_player)
@@ -18,3 +20,8 @@ func _on_body_entered(body):
 		)
 		
 		queue_free()
+	if coins != original_coins:
+		coin_collected.emit(coins)
+		
+func get_coins():
+	return coins
